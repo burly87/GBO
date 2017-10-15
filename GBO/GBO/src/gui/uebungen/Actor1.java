ad67f7f5e25d9e52a991c319fbae7ca70c9cd440
burly87 <31670630+burly87@users.noreply.github.com>
2017-10-15 18:51:06 +0200

    Aufgabe 1.1 fertig
    addROle und clearRole eingefügt

--- GBO/GBO/src/gui/uebungen/Actor1.java
+++ GBO/GBO/src/gui/uebungen/Actor1.java
@@ -12,6 +12,10 @@ public class Actor1 extends Application
 
     private Role1 role;
 
+    private static int counter = 0;
+
+    private static ArrayList<Role1> addRole = new ArrayList<Role1>();
+
     public void action()
     {
         if (role != null)
@@ -26,29 +30,34 @@ public class Actor1 extends Application
 
     public void addRole(Role1 role1)
     {
-        ArrayList<Role1> roleFICKDICH = new ArrayList<Role1>();
-        roleFICKDICH.add(role1);
-        roleFICKDICH.add(new ByeRole());
-        // roleFICKDICH.add(new GeneralRole("bla"));
-        System.out.println(roleFICKDICH.toString());
+        counter++;
+        addRole.add(role1);
+        // System.out.println(addRole.toString());
     }
 
-    public void clearRole()
+    public static void clearRole()
     {
-
+        addRole.clear();
+        System.out.println("clear wurde ausgeführt");
     }
 
     public static void main(String[] args)
     {
         Actor1 actor = new Actor1();
         actor.action();
-
         actor.addRole(new Hallo());
         actor.action();
         actor.addRole(new ByeRole());
         actor.action();
-        actor.addRole(new GeneralRole("Das ist ein test"));
+        actor.addRole(new Hallo());
         actor.action();
+        actor.addRole(new GeneralRole("I'll be back!"));
+        actor.action();
+
+        System.out.println("Anzahl an Rollen in ArrayList: " + counter);
+        clearRole();
+        // System.out.println("Anzahl an Rollen nach CLEAR:" +
+        // addRole.toString());
 
         // launch(args);
     }

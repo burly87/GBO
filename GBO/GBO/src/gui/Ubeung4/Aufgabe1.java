bee9c79774f3a7815c289097a8b6c052d09b8110
burly87 <31670630+burly87@users.noreply.github.com>
2017-11-06 20:22:31 +0100

    Uebung4 added

--- GBO/GBO/src/gui/Ubeung4/Aufgabe1.java
+++ GBO/GBO/src/gui/Ubeung4/Aufgabe1.java
@@ -1,6 +1,34 @@
 package gui.Ubeung4;
 
-public class Aufgabe1
-{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Aufgabe1 extends Application
+{
+    public void start(Stage primaryStage)
+    {
+        Pane root = new Pane();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Button b = new Button("Button " + i);
+            b.setLayoutX(i * 20);
+            b.setLayoutY(i * 20);
+            root.getChildren().add(b);
+        }
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Pane-Beispiel");
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }

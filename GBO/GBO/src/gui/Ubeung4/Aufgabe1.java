859ed2e67d1a63dba71f282e12060e7204b7519f
ForveT <ForveT@users.noreply.github.com>
2017-11-07 16:57:31 +0100

    Aufgabe 4 Update

--- GBO/GBO/src/gui/Ubeung4/Aufgabe1.java
+++ GBO/GBO/src/gui/Ubeung4/Aufgabe1.java
@@ -1,6 +1,8 @@
 package gui.Ubeung4;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
@@ -8,23 +10,70 @@ import javafx.stage.Stage;
 
 public class Aufgabe1 extends Application
 {
+
     public void start(Stage primaryStage)
     {
         Pane root = new Pane();
 
-        for (int i = 1; i <= 10; i++)
+        root.widthProperty().addListener(new ChangeListener<Number>()
         {
-            Button b = new Button("Button " + i);
-            b.setLayoutX(i * 20);
-            b.setLayoutY(i * 20);
-            root.getChildren().add(b);
-        }
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldSize, Number newSize)
+            {
+
+                changeSize(root);
+
+            }
+
+        });
+
+        root.heightProperty().addListener(new ChangeListener<Number>()
+        {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldSize, Number newSize)
+            {
+                changeSize(root);
+
+            }
+
+        });
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Pane-Beispiel");
         primaryStage.show();
 
+        Button start = (Button) root.getChildren();
+
+    }
+
+    double lerp(double v0, double v1, double t)
+    {
+        return v0 + t * (v1 - v0);
+    }
+
+    public void changeSize(Pane root)
+    {
+
+        Number width = root.widthProperty().getValue();
+        Number height = root.heightProperty().getValue();
+
+        // double oldBtnX = btn.getWidth();
+        // double oldBtnY = btn.gehtHeight();
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Button btn = (Button) root.getChildren().get(i);
+            btn.setLayoutX(lerp(0, (double) width, i));
+            btn.setLayoutY(lerp(0, (double) height, i));
+
+            root.getChildren().add(btn);
+        }
+
+        System.out.println("width:" + width);
+        System.out.println("height:" + height);
     }
 
     public static void main(String[] args)

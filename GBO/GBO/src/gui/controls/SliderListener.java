7cff12c51654df200cb448ff563d490f960c85b2
burly87 <31670630+burly87@users.noreply.github.com>
2017-11-19 13:11:48 +0100

    ASB Aufgaben weiter gemacht

---
+++ GBO/GBO/src/gui/controls/SliderListener.java
@@ -0,0 +1,53 @@
+package gui.controls;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class SliderListener extends Application
+{
+
+    public void start(Stage primaryStage)
+    {
+        GridPane grid = new GridPane();
+
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10));
+
+        Slider slider = new Slider(0, 100, 50);
+        Label label = new Label("Slider wurde noch nicht bewegt");
+
+        slider.valueProperty().addListener(new ChangeListener<Number>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+            {
+                double sum = 0.0;
+                sum = (double) oldValue - (double) newValue;
+                String sum2 = Double.toString(sum);
+                label.setText("Aenderung des Sliders um " + sum2);
+            }
+        });
+
+        grid.add(slider, 0, 0);
+        grid.add(label, 0, 1);
+
+        Scene scene = new Scene(grid, 480, 80);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Slider mit Listener");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+}
--- GBO/GBO/src/gui/country/combo/CountryInfo.java
+++ GBO/GBO/src/gui/country/combo/CountryInfo.java
@@ -6,11 +6,16 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -24,14 +29,18 @@ public class CountryInfo extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception
-    {
+    { /* Surface */
         GridPane root = new GridPane();
 
         VBox vboxLeft = new VBox(5);
         VBox vboxRight = new VBox(5);
-        root.add(new VBox(vboxLeft), 0, 2);
-        root.add(new VBox(vboxRight), 1, 2);
+        HBox hBox = new HBox(2);
 
+        root.add(vboxLeft, 0, 2);
+        root.add(vboxRight, 1, 2);
+        root.add(hBox, 0, 3, 5, 1);
+
+        /* in Boxes */
         Label countryName = new Label("Land:");
         Label capital = new Label("Hauptstadt:");
         Label population = new Label("Einwohner:");
@@ -44,21 +53,45 @@ public class CountryInfo extends Application
         Label areaOut = new Label();
         Label densityOut = new Label();
 
+        TextField tfCountry = new TextField("Land");
+        TextField tfCapital = new TextField("Hauptstadt");
+        TextField tfPopulation = new TextField("Einwohner");
+        TextField tfArea = new TextField("Flaeche");
+
+        Button add = new Button("Hinzufuegen");
+        Button delete = new Button("Loeschen");
+
         /* ComboBox anlegen mit entsprechendem Listener */
         cEnum.setId("countrySeletor");
         // cEnum.setValue(countrys.get(0));
-
         cEnum.setEditable(false);
 
-        cEnum.setOnAction((e) ->
+        cEnum.setOnAction(new EventHandler<ActionEvent>()
         {
-            Country name = cEnum.getSelectionModel().getSelectedItem();
-            cEnum.setPromptText(name.getName());
-            countryOut.setText(name.getName());
-            capitalOut.setText(name.getCapital());
-            populationOut.setText("" + name.getPeople());
-            areaOut.setText("" + name.getArea());
-            densityOut.setText("" + (name.getPeople()) / (name.getArea()));
+            @Override
+            public void handle(ActionEvent event)
+            {
+                Country name = cEnum.getSelectionModel().getSelectedItem();
+                cEnum.setPromptText(name.getName());
+                countryOut.setText(name.getName());
+                capitalOut.setText(name.getCapital());
+                populationOut.setText("" + name.getPeople());
+                areaOut.setText("" + name.getArea());
+                densityOut.setText("" + (name.getPeople()) / (name.getArea()));
+
+                long roundedPop = name.getPeople();
+                long roundedArea = name.getArea();
+
+                if (roundedPop >= 999999)
+                {
+                    populationOut.setText("" + name.getPeople() / 1000000 + " Mill.");
+                }
+                if (roundedArea >= 999999)
+                {
+                    areaOut.setText("" + name.getArea() / 1000000 + " Mill.");
+                }
+
+            }
         });
 
         /* CheckBox */
@@ -67,17 +100,68 @@ public class CountryInfo extends Application
 
         checkBox.setOnAction((e) ->
         {
-            roundOff();
+            Country name = cEnum.getSelectionModel().getSelectedItem();
+            populationOut.setText("" + name.getPeople());
+            areaOut.setText("" + name.getArea());
+
+            long roundedPop = name.getPeople();
+            long roundedArea = name.getArea();
+
+            if (roundedPop >= 999999 && checkBox.isSelected() == false)
+            {
+                populationOut.setText("" + name.getPeople() / 1000000 + " Mill.");
+            }
+            if (roundedArea >= 999999 && checkBox.isSelected() == false)
+            {
+                areaOut.setText("" + name.getArea() / 1000000 + " Mill.");
+            }
+
+        });
+
+        /* ButtonListener */
+        add.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                String strCountry = tfCountry.getText();
+                String strCapital = tfCapital.getText();
+                String strPopulation = tfPopulation.getText();
+                String strArea = tfArea.getText();
+                long lPopulation = Long.parseLong(strPopulation);
+                long lArea = Long.parseLong(strArea);
+
+                if (!"".equals(tfCountry.getText()) && (!"".equals(tfCapital.getText())))
+                {
+                    cEnum.getItems().add(new Country(strCountry, strCapital, lPopulation, lArea));
+                }
+
+            }
+        });
+
+        delete.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                Country selected = cEnum.getSelectionModel().getSelectedItem();
+                cEnum.getItems().remove(selected);
+
+                if (!"".equals(cEnum.getItems()))
+                {
+                    cEnum.setPromptText("Keine Laender vorhanden");
+                }
+            }
         });
 
         /* Zeichnen */
         root.getChildren().add(cEnum);
         root.add(checkBox, 0, 1);
-
         vboxLeft.getChildren().addAll(countryName, capital, population, area, density);
         vboxRight.getChildren().addAll(countryOut, capitalOut, populationOut, areaOut, densityOut);
+        hBox.getChildren().addAll(tfCountry, tfCapital, tfPopulation, tfArea, add, delete);
 
-        Scene scene = new Scene(root, 400, 250);
+        Scene scene = new Scene(root, 600, 250);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Länder-Informationen");
         primaryStage.show();
@@ -87,12 +171,12 @@ public class CountryInfo extends Application
     public void roundOff()
     {
         Country erg = cEnum.getSelectionModel().getSelectedItem();
+
         long rounded = erg.getPeople();
 
         if (rounded >= 999999)
         {
             rounded /= 1000000;
-            System.out.println(rounded);
         }
 
     }
